acc1e62296517c75147d91e79731df090749f67d
Dale Taylor <applestuff@nimby.net>
2017-06-14 17:44:24 -0600

    changed pre-processing for better lda

--- lda_model.py
+++ lda_model.py
@@ -1,6 +1,3 @@
-'''uses saved lda model and dictionary from gensim'''
-
-#https://rstudio-pubs-static.s3.amazonaws.com/79360_850b2a69980c4488b1db95987a24867a.html
 import os
 import time
 import glob
@@ -11,75 +8,82 @@ import re
 
 from gensim import corpora, models
 from gensim.utils import smart_open, simple_preprocess
-from gensim.corpora.wikicorpus import _extract_pages, filter_wiki
 from gensim.parsing.preprocessing import STOPWORDS
 
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.cluster import KMeansClusterer, euclidean_distance
 from gensim import corpora, models, utils
+from gensim import utils
+import gensim.parsing
 
 
+def tokenize(text, stopwords=STOPWORDS):
+    return [token for token in simple_preprocess(text) if token not in STOPWORDS]
 
 
+from nltk import WordNetLemmatizer
+from multiprocessing import Pool
+#https://stackoverflow.com/a/38020540
+def lemmed(text, cores=2): # tweak cores as needed
+    with Pool(processes=cores) as pool:
+        wnl = WordNetLemmatizer()
+        result = pool.map(wnl.lemmatize, text)
+    return result
 
-def preprocess(document, stop, stemmer, tokenizer, add_stop=None, remove_digits=True, remove_lf=False) :
-    document = document.lower()
-    if remove_digits:
-        processed = re.sub("\d+", "", document)
-    processed = tokenizer.tokenize(processed)
-    processed = [word for word in processed if word not in stop]
-    if add_stop is not None :
-        processed = [stemmer.stem(word) for word in processed if word not in add_stop]
-    else :
-        processed = [stemmer.stem(word) for word in processed]
-    if remove_lf :
-        #we can remove low frequency in gensim dictionary object
-        pass
-    return processed
+
+def preprocess(text, stopwords=STOPWORDS, cores=2) :
+    return lemmed(tokenize(text,stopwords=stopwords),cores=cores)
+
+def create_denver1000_stopwords():
+    '''
+    We are tying to find coherence/decoherence across reviews with LDA, so we are
+    greatly expanding stop words to include all the names, neighborhoods,
+    cuisines, etc. from the yelp Denver 1000 restaurants, we are pulling these
+    directly from the our biz_details data csv. Neighborhoods pulled from yelp.
+    Adding back 'new, old, fast, slow' as these important to reviews, not place.
+    '''
+    locations = ['rino','lodo','alamo', 'auraria', 'baker', 'capitol', 'cbd', 'cherry', 'city', 'club', 'congress', 'country', 'creek', 'curtis', 'east', 'five', 'golden', 'highland', 'hill', 'jefferson', 'lincoln', 'lodo', 'north', 'northeast', 'northwest', 'park', 'placita', 'points', 'south', 'southeast', 'southwest', 'speer', 'stapleton', 'triangle', 'university', 'uptown', 'washington', 'west']
+
+    bf = pd.read_csv('biz_details.csv')
+    food_stop = list(bf['servesCuisine'].dropna().unique())
+    address_stop = list(bf['streetAddress'].dropna().unique())
+    address_stop +=locations
+    title_stop = list(bf['name'].dropna().unique())
+    add_stop_temp = set(food_stop + address_stop + title_stop)
+    add_stop = preprocess(' '.join(add_stop_temp), stopwords=STOPWORDS, cores=2)
+    add_back = ['new', 'old', 'fast', 'slow']
+    add_stop = set([word for word in add_stop if len(word)>2 and word not in add_back])
+    with open('grelp-me/data/yelp_stop.txt', 'w') as f :
+        f.write(','.join(add_stop))
+
+def load_denver1000_stopwords():
+    with open('data/yelp_stop.txt', 'r') as f :
+        return f.read().split(',')
 
 if __name__ == '__main__':
+    pass
 
-    from nltk.tokenize import RegexpTokenizer
-    from stop_words import get_stop_words
-    from nltk.stem.porter import PorterStemmer
-    from nltk.stem.lancaster import LancasterStemmer
-
-
-    #l_stemmer = LancasterStemmer()
-    stemmer = PorterStemmer()
-    stop = get_stop_words('en')
-    tokenizer = RegexpTokenizer(r'\w+')
-
-    #We want to get rid of cuisines, neighborhoods, titles of restaurtant
-    def create_denver1000_stopwords():
-        '''
-        We are tying to find coherence/decoherence across reviews with LDA, so we are
-        greatly expanding stop words to include all the names, neighborhoods,
-        cuisines, etc. from the yelp Denver 1000 restaurants, we are pulling these
-        directly from the our biz_details data csv. Neighborhoodd pulled from yelp.
-        '''
-        locations = ['rino','lodo','alamo', 'auraria', 'baker', 'capitol', 'cbd', 'cherry', 'city', 'club', 'congress', 'country', 'creek', 'curtis', 'east', 'five', 'golden', 'highland', 'hill', 'jefferson', 'lincoln', 'lodo', 'north', 'northeast', 'northwest', 'park', 'placita', 'points', 'south', 'southeast', 'southwest', 'speer', 'stapleton', 'triangle', 'university', 'uptown', 'washington', 'west']
-
-        bf = pd.read_csv('../biz_details.csv')
-        food_stop = list(bf['servesCuisine'].unique())
-        address_stop = list(bf['streetAddress'].unique())
-        address_stop +=locations
-        title_stop = list(bf['name'].unique())
-        add_stop_temp = set(food_stop + address_stop + title_stop)
-        add_stop = preprocess(str(add_stop_temp), stop, stemmer, tokenizer)
-        add_back = ['new', 'old', 'fast', 'slow']
-        add_stop = set([word for word in add_stop if len(word)>2 and word not in add_back])
-
-        with open('data/yelp_stop.txt', 'w') as f :
-            f.write(','.join(add_stop))
-
-    def load_denver1000_stopwords():
-        with open('data/yelp_stop.txt', 'r') as f :
-            devner1000words = f.read().split(',')
+    # from nltk.tokenize import RegexpTokenizer
+    # from stop_words import get_stop_words
+    # from nltk.stem.porter import PorterStemmer
+    # from nltk.stem.lancaster import LancasterStemmer
+    #
+    #
+    # #l_stemmer = LancasterStemmer()
+    # stemmer = PorterStemmer()
+    # stop = get_stop_words('en')
+    # tokenizer = RegexpTokenizer(r'\w+')
+    # denver1000_stopwords = load_denver1000_stopwords()
+    # add_stop = denver1000_stopwords
+    #
+    # df = pd.read_csv('../reviews.csv')
+    # document = df.ix[0, 'review']
+    # p = preprocess(document, stop, stemmer, tokenizer, add_stop=add_stop)
 
 
-"/Users/d4/Dropbox/dsi/yelp/lda/reviews.dict"
+
+
 #dictionary.load('../lda/reviews.dict')
 
 #df = pd.read_csv('reviews.csv')
